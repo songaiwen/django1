c5c1c9a9bf50a95c5888fc2594793f82480a16c4
songaiwen <576883213@qq.com>
2018-06-11 14:32:12 +0800

    创建项目建立视图配置url响应模板

--- test1/booktest/models.py
+++ test1/booktest/models.py
@@ -1,3 +1,21 @@
+#coding=utf8
 from django.db import models
 
 # Create your models here.
+class BookInfo(models.Model):
+    title = models.CharField(max_length=20)
+    pub_date = models.DateField()
+
+    def __str__(self):
+        return self.title.encode('utf-8')
+
+class HeroInfo(models.Model):
+    name = models.CharField(max_length=20)
+    content = models.CharField(max_length=100)
+    gender = models.BooleanField(default=True)
+    #设置的外键
+    book = models.ForeignKey(BookInfo)
+
+    def __str__(self):
+        return self.name.encode('utf-8')
+
--- test1/booktest/views.py
+++ test1/booktest/views.py
@@ -1,3 +1,30 @@
+#coding=utf-8
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from models import *
+
+
+def index(request):
+
+    # context = {
+    #     'title':'django首页',
+    #     'list':range(10)
+    # }
+    # return render(request, 'booktest/index.html',context)
+    # return HttpResponse('hello world')
+    list = BookInfo.objects.all()
+    context = {
+        'booklist':list
+    }
+    return render(request,'booktest/index2.html',context)
+
+
+def detail(request,id):
+    list = BookInfo.objects.get(id=id).heroinfo_set.all()
+    context = {
+        'herolist':list
+
+    }
+    return render(request,'booktest/detail.html',context)
+
